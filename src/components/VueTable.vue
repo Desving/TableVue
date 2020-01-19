6460a1f4c0933a660a6b8c3e5b506e909752f35f
Denis <shergin1995@gmail.com>
2020-01-19 23:01:58 +0300

    Вынес строки в отдельный компонент и мелкий рефакторинг

--- src/components/VueTable.vue
+++ src/components/VueTable.vue
@@ -1,40 +1,22 @@
 <template>
     <table class="table mt-3">
         <thead>
-        <tr v-bind:class="addExtraClassHeader">
+        <tr :class="addExtraClassHeader">
             <th>#</th>
-            <th v-bind:key="keyHeadCell" v-for="(cellHeadName, keyHeadCell) in fields">{{ cellHeadName }}</th>
+            <th :key="keyHeadCell" v-for="(cellHeadName, keyHeadCell) in fields">{{ cellHeadName }}</th>
             <th>actions</th>
         </tr>
         </thead>
         <tbody>
-        <tr v-bind:key="keyRow" v-for="(row, keyRow) in getViewChunkRows" v-bind:class="addExtraClassRow(keyRow)">
-            <th scope="row">{{ keyRow + 1 }}</th>
-            <td v-for="fieldName in fields"
-                v-if="fieldName in row"
-                @dblclick="changeCell(keyRow, fieldName, row[fieldName])"
-            >
-                <input class="form-control" type="text" v-if="isChangeCell(keyRow, fieldName)"
-                       v-model="valueCell"
-                       @keyup.esc="resetChangeCell"
-                       @blur="saveValueCell"
-                       @keyup.enter="saveValueCell"
-                />
-                <div v-else>{{row[fieldName]}}</div>
-            </td>
-            <td v-else> No data</td>
-            <td class="d-flex justify-content-center">
-                <button type="button" class="btn">
-                    <span aria-hidden="true" @click="addRowByKey(keyRow + 1)">&#8744;</span>
-                </button>
-                <button type="button" class="btn">
-                    <span aria-hidden="true" @click="addRowByKey(keyRow)">&#8743;</span>
-                </button>
-                <button type="button" class="close">
-                    <span aria-hidden="true" @click="deleteRows(keyRow)">&times;</span>
-                </button>
-            </td>
-        </tr>
+            <row-table :key="keyRow" v-for="(row, keyRow) in getViewChunkRows"
+                       :fields-value="row"
+                       :fields-name="fields"
+                       :key-row="keyRow"
+                       :even-class-name="meta.even"
+                       :odd-class-name="meta.odd"
+                       @deleteRow='onDeleteRow'
+                       @addRowByKey='onAddRowByKey'
+            ></row-table>
         </tbody>
         <tfoot>
         <tr>
@@ -48,10 +30,11 @@
                 <button class="btn btn-light" @click="copyTable">Копирование данных</button>
             </td>
             <td colspan="4">
-                <ul class="pagination d-flex justify-content-center">
+                <ul class="pagination d-flex justify-content-center"
+                    v-if="getCountPages > 1">
                     <li class="page-item" v-for="pageNumber in getCountPages"
-                        v-if="getCountPages > 1"
-                        v-bind:class="currentPage==pageNumber?'active':''">
+                        :key="pageNumber"
+                        :class="currentPage==pageNumber?'active':''">
                         <button class="page-link" @click="changeCurrentPage(pageNumber)">
                             {{ pageNumber }}
                         </button>
@@ -64,8 +47,13 @@
 </template>
 
 <script>
+    import RowTable from "./RowTable";
+
     export default {
         name: 'VueTable',
+        components: {
+            RowTable
+        },
         props: {
             'fields': {
                 'type': Array,
@@ -79,7 +67,7 @@
             },
             'meta': {
                 'type': Object,
-                'default': 0,
+                'default': {},
                 'required': true
             },
             'keyTable': {
@@ -95,56 +83,28 @@
             return {
                 countPerPage: 10,
                 currentPage: 1,
-                keyChangeRow: 0,
-                nameChangeField: '',
-                valueCell: '',
-                defaultValueForNewRows: 'default'
             }
         },
         methods: {
+            onDeleteRow(keyRow) {
+                this.rows.splice(keyRow, 1)
+            },
+            onAddRowByKey(data) {
+                let keyRow = data.keyRow * this.currentPage;
+                let newRows = {};
+                this.fields.forEach((element) => {
+                    newRows[element] = data.defaultValueForNewRows;
+                });
+                this.rows.splice(keyRow, 0, newRows);
+            },
             cleanTable() {
                 this.$store.dispatch('cleanTableByKey', this.keyTable);
             },
             deleteTable() {
                 this.$store.dispatch('deleteTableByKey', this.keyTable);
             },
-            deleteRows(keyRows) {
-                this.rows.splice(keyRows, 1)
-            },
             changeCurrentPage(pageNumber) {
                 this.currentPage = pageNumber;
-            },
-            changeCell(keyRow, fieldName, valueCell) {
-                this.keyChangeRow = keyRow;
-                this.nameChangeField = fieldName;
-                this.valueCell = valueCell;
-            },
-            isChangeCell(keyRow, fieldName) {
-                return fieldName === this.nameChangeField && keyRow === this.keyChangeRow;
-            },
-            saveValueCell() {
-                this.rows[this.keyChangeRow][this.nameChangeField] = this.valueCell;
-                this.resetChangeCell()
-            },
-            resetChangeCell() {
-                this.keyChangeRow = 0;
-                this.nameChangeField = '';
-                this.valueCell = '';
-            },
-            addRowByKey(key) {
-                let keyRow = key * this.currentPage;
-                let newRows = {};
-                this.fields.forEach((element) => {
-                    newRows[element] = this.defaultValueForNewRows;
-                });
-                this.rows.splice(keyRow, 0, newRows);
-            },
-            addExtraClassRow(keyRow) {
-                if (keyRow % 2 === 0 && this.meta.even) {
-                    return this.meta.even
-                } else if (keyRow % 2 !== 0 && this.meta.odd) {
-                    return this.meta.odd
-                }
             },
             async copyTable() {
                 let objToCopy = {
@@ -166,6 +126,7 @@
                 if (this.rows) {
                     return Math.ceil(this.rows.length / this.countPerPage)
                 }
+                return 0;
             },
             getViewChunkRows() {
                 let start = this.countPerPage * (this.currentPage - 1);
